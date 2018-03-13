be2e46d7195cd5b91558feaefab4b4b509f09b9d
Onkar Patil <onkarpatil1945@gmail.com>
2018-03-13 07:26:27 +0530

    Initial commit

---
+++ tic tac toe/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.A1 = New System.Windows.Forms.Button()
+        Me.A2 = New System.Windows.Forms.Button()
+        Me.A3 = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.C1 = New System.Windows.Forms.Button()
+        Me.C2 = New System.Windows.Forms.Button()
+        Me.C3 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'A1
+        '
+        Me.A1.Location = New System.Drawing.Point(12, 37)
+        Me.A1.Name = "A1"
+        Me.A1.Size = New System.Drawing.Size(54, 56)
+        Me.A1.TabIndex = 0
+        Me.A1.UseVisualStyleBackColor = True
+        '
+        'A2
+        '
+        Me.A2.Location = New System.Drawing.Point(72, 37)
+        Me.A2.Name = "A2"
+        Me.A2.Size = New System.Drawing.Size(54, 56)
+        Me.A2.TabIndex = 1
+        Me.A2.UseVisualStyleBackColor = True
+        '
+        'A3
+        '
+        Me.A3.Location = New System.Drawing.Point(132, 37)
+        Me.A3.Name = "A3"
+        Me.A3.Size = New System.Drawing.Size(54, 56)
+        Me.A3.TabIndex = 2
+        Me.A3.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(12, 100)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(54, 56)
+        Me.B1.TabIndex = 3
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Location = New System.Drawing.Point(72, 99)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(54, 56)
+        Me.B2.TabIndex = 4
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Location = New System.Drawing.Point(132, 100)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(54, 56)
+        Me.B3.TabIndex = 5
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'C1
+        '
+        Me.C1.Location = New System.Drawing.Point(12, 162)
+        Me.C1.Name = "C1"
+        Me.C1.Size = New System.Drawing.Size(54, 56)
+        Me.C1.TabIndex = 6
+        Me.C1.UseVisualStyleBackColor = True
+        '
+        'C2
+        '
+        Me.C2.Location = New System.Drawing.Point(72, 162)
+        Me.C2.Name = "C2"
+        Me.C2.Size = New System.Drawing.Size(54, 56)
+        Me.C2.TabIndex = 7
+        Me.C2.UseVisualStyleBackColor = True
+        '
+        'C3
+        '
+        Me.C3.Location = New System.Drawing.Point(132, 162)
+        Me.C3.Name = "C3"
+        Me.C3.Size = New System.Drawing.Size(54, 56)
+        Me.C3.TabIndex = 8
+        Me.C3.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(200, 24)
+        Me.MenuStrip1.TabIndex = 9
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewGameToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'NewGameToolStripMenuItem
+        '
+        Me.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem"
+        Me.NewGameToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NewGameToolStripMenuItem.Text = "New Game"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(200, 237)
+        Me.Controls.Add(Me.C3)
+        Me.Controls.Add(Me.C2)
+        Me.Controls.Add(Me.C1)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.A3)
+        Me.Controls.Add(Me.A2)
+        Me.Controls.Add(Me.A1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents A1 As System.Windows.Forms.Button
+    Friend WithEvents A2 As System.Windows.Forms.Button
+    Friend WithEvents A3 As System.Windows.Forms.Button
+    Friend WithEvents B1 As System.Windows.Forms.Button
+    Friend WithEvents B2 As System.Windows.Forms.Button
+    Friend WithEvents B3 As System.Windows.Forms.Button
+    Friend WithEvents C1 As System.Windows.Forms.Button
+    Friend WithEvents C2 As System.Windows.Forms.Button
+    Friend WithEvents C3 As System.Windows.Forms.Button
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NewGameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ tic tac toe/Form1.vb
@@ -0,0 +1,272 @@
+﻿Public Class Form1
+    Dim turn As Boolean = True
+    Dim turn_count As Integer = 0
+    Private Sub Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles A1.Click, A2.Click, A3.Click, C3.Click, C2.Click, C1.Click, B3.Click, B2.Click, B1.Click
+        Dim bttn As Button = sender
+        Dim againstcomp As Boolean = True
+        If (turn) Then
+            bttn.Text = "X"
+        Else
+            bttn.Text = "O"
+        End If
+        turn = Not turn
+        bttn.Enabled = False
+        turn_count = turn_count + 1
+        CheckBo()
+        If (Not turn And againstcomp) Then
+            computer_make_move()
+        End If
+    End Sub
+    Private Function CheckBo()
+        Dim winner As Boolean = False
+        Dim ans As String
+        If ((A1.Text = A2.Text) And (A2.Text = A3.Text) And (Not A1.Enabled)) Then
+            winner = True
+        ElseIf ((B1.Text = B2.Text) And (B2.Text = B3.Text) And (Not B1.Enabled)) Then
+            winner = True
+        ElseIf ((C1.Text = C2.Text) And (C2.Text = C3.Text) And (Not C1.Enabled)) Then
+            winner = True
+        ElseIf ((A1.Text = B1.Text) And (B1.Text = C1.Text) And (Not A1.Enabled)) Then
+            winner = True
+        ElseIf ((A2.Text = B2.Text) And (B2.Text = C2.Text) And (Not A2.Enabled)) Then
+            winner = True
+        ElseIf ((A3.Text = B3.Text) And (B3.Text = C3.Text) And (Not A3.Enabled)) Then
+            winner = True
+        ElseIf ((A1.Text = B2.Text) And (B2.Text = C3.Text) And (Not A1.Enabled)) Then
+            winner = True
+        ElseIf ((A3.Text = B2.Text) And (B2.Text = C1.Text) And (Not C1.Enabled)) Then
+            winner = True
+        End If
+        If (winner) Then
+            disable()
+            If (turn) Then
+                ans = "O"
+            Else
+                ans = "X"
+            End If
+            MessageBox.Show(ans + " is winner")
+        ElseIf turn_count = 9 Then
+            MessageBox.Show("Draw!! No one wins")
+        End If
+
+    End Function
+
+    Private Function disable()
+        Dim c As Control
+        Dim btn As Button
+        Try
+            For Each c In Controls
+                btn = c
+                btn.Enabled = False
+            Next
+        Catch
+        End Try
+    End Function
+
+    Private Sub NewGameToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NewGameToolStripMenuItem.Click
+        Dim c As Control
+        turn = True
+        turn_count = 0
+        Dim btn As Button
+        Try
+            For Each c In Controls
+                btn = c
+                btn.Enabled = True
+                btn.Text = ""
+            Next
+        Catch
+        End Try
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Multiplayer tic tac toe game")
+    End Sub
+
+    Private Function computer_make_move()
+        Dim move As Button = Nothing
+        Move = look_for_win_or_block("O")
+        If (move Is Nothing) Then
+            move = look_for_win_or_block("X")
+            If (move Is Nothing) Then
+                move = look_for_corner()
+                If (move Is Nothing) Then
+                    move = look_for_open_space()
+                End If
+            End If
+        End If
+        Try
+            move.PerformClick()
+        Catch null As NullReferenceException
+        End Try
+    End Function
+
+    Private Function look_for_open_space() As Button
+        Console.WriteLine("Looking for open space")
+        Dim b As Button = Nothing
+        Dim c As Control
+        Try
+            For Each c In Controls
+                b = c
+                If b IsNot Nothing Then
+                    If b.Text = "" Then
+                        Return b
+                    End If
+                End If
+            Next
+        Catch
+        End Try
+        Return Nothing
+    End Function
+
+
+    Private Function look_for_corner() As Button
+        Console.WriteLine("Looking for corner")
+        If A1.Text = "O" Then
+            If (A3.Text = "") Then
+                Return A3
+            End If
+            If (C3.Text = "") Then
+                Return C3
+            End If
+            If (C1.Text = "") Then
+                Return C1
+            End If
+        End If
+
+        If (A3.Text = "O") Then
+            If (A1.Text = "") Then
+                Return A1
+            End If
+            If (C3.Text = "") Then
+                Return C3
+            End If
+            If (C1.Text = "") Then
+                Return C1
+            End If
+        End If
+
+        If (C3.Text = "O") Then
+
+            If (A1.Text = "") Then
+                Return A3
+            End If
+            If (A3.Text = "") Then
+                Return A3
+            End If
+            If (C1.Text = "") Then
+                Return C1
+            End If
+        End If
+
+        If (C1.Text = "O") Then
+            If (A1.Text = "") Then
+                Return A3
+            End If
+            If (A3.Text = "") Then
+                Return A3
+            End If
+            If (C3.Text = "") Then
+                Return C3
+            End If
+        End If
+
+        If (A1.Text = "") Then
+            Return A1
+        End If
+        If (A3.Text = "") Then
+            Return A3
+        End If
+        If (C1.Text = "") Then
+            Return C1
+        End If
+        If (C3.Text = "") Then
+            Return C3
+        End If
+        Return Nothing
+    End Function
+
+    Private Function look_for_win_or_block(ByVal mark As String) As Button
+        Console.WriteLine("Looking for win or block:  " + mark)
+        If ((A1.Text = mark) And (A2.Text = mark) And (A3.Text = "")) Then
+            Return A3
+        End If
+        If ((A2.Text = mark) And (A3.Text = mark) And (A1.Text = "")) Then
+            Return A1
+        End If
+        If ((A1.Text = mark) And (A3.Text = mark) And (A2.Text = "")) Then
+            Return A2
+        End If
+        If ((B1.Text = mark) And (B2.Text = mark) And (B3.Text = "")) Then
+            Return B3
+        End If
+        If ((B2.Text = mark) And (B3.Text = mark) And (B1.Text = "")) Then
+            Return B1
+        End If
+        If ((B1.Text = mark) And (B3.Text = mark) And (B2.Text = "")) Then
+            Return B2
+        End If
+        If ((C1.Text = mark) And (C2.Text = mark) And (C3.Text = "")) Then
+            Return C3
+        End If
+        If ((C2.Text = mark) And (C3.Text = mark) And (C1.Text = "")) Then
+            Return C1
+        End If
+        If ((C1.Text = mark) And (C3.Text = mark) And (C1.Text = "")) Then
+            Return C2
+        End If
+
+
+        If ((A1.Text = mark) And (B1.Text = mark) And (C1.Text = "")) Then
+            Return C1
+        End If
+        If ((B1.Text = mark) And (C1.Text = mark) And (A1.Text = "")) Then
+            Return A1
+        End If
+        If ((A1.Text = mark) And (C1.Text = mark) And (B1.Text = "")) Then
+            Return B1
+        End If
+        If ((A2.Text = mark) And (B2.Text = mark) And (C2.Text = "")) Then
+            Return C2
+        End If
+        If ((B2.Text = mark) And (C2.Text = mark) And (A2.Text = "")) Then
+            Return A2
+        End If
+        If ((A2.Text = mark) And (C2.Text = mark) And (B2.Text = "")) Then
+            Return B2
+        End If
+        If ((A3.Text = mark) And (B3.Text = mark) And (C3.Text = "")) Then
+            Return C3
+        End If
+        If ((B3.Text = mark) And (C3.Text = mark) And (A3.Text = "")) Then
+            Return A3
+        End If
+        If ((A3.Text = mark) And (C3.Text = mark) And (B3.Text = "")) Then
+            Return B3
+        End If
+
+        If ((A1.Text = mark) And (B2.Text = mark) And (C3.Text = "")) Then
+            Return C3
+        End If
+        If ((B2.Text = mark) And (C3.Text = mark) And (A1.Text = "")) Then
+            Return A1
+        End If
+        If ((A1.Text = mark) And (C3.Text = mark) And (B2.Text = "")) Then
+            Return B2
+        End If
+        If ((A3.Text = mark) And (B2.Text = mark) And (C1.Text = "")) Then
+            Return C1
+        End If
+        If ((B2.Text = mark) And (C1.Text = mark) And (A3.Text = "")) Then
+            Return A3
+        End If
+        If ((A3.Text = mark) And (C1.Text = mark) And (B2.Text = "")) Then
+            Return B2
+        End If
+            Return Nothing
+    End Function
+
+End Class
